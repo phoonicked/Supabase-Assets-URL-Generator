04dce78491de2321ee8f3c2fb203d2084735f982
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-07-15 11:33:24 +0100

    Final public version

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 import json
 from supabase import create_client, Client
 
-# Supabase credentials
-supabase_url = 'https://siuhxcpfdrwjqmtecjle.supabase.co'
-supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpdWh4Y3BmZHJ3anFtdGVjamxlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDA0MDM5NCwiZXhwIjoyMDM1NjE2Mzk0fQ.F-z6ZBBhy321G3QHQwhPvsX2I5JdLFBhgqrga6sqVTw'  # Replace with your Service Role key
+
+supabase_url = ''  # Replace with Supabase URL
+supabase_key = ''  # Replace with Supabase Service Role key
 supabase: Client = create_client(supabase_url, supabase_key)
 
 def generate_public_urls(file_names):
@@ -31,8 +31,7 @@ def generate_public_urls(file_names):
 
 if __name__ == '__main__':
     while True:
-        # Prompt the user for file names
-        file_names_input = input("Enter the file names separated by commas (e.g., graphics/03.01.05.jpg, graphics/03.01.06.jpg) or press Enter to stop: ")
+        file_names_input = input("Enter the file names separated by commas (e.g., 03.01.05.jpg, 03.01.06.jpg) or press Enter to stop: ")
         if not file_names_input.strip():
             print("No input provided. Exiting...")
             break
